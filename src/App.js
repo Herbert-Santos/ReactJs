ef941e45abd9bdbaa835137b6595d948948a888d
Hebert Santos <hebertsantos@Air-de-Hebert.home>
2021-06-19 23:38:02 -0300

    Componentizacao jsx

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from './components/header';
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Header />
+            <Header />
+
+        </>
+        )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const soma = (a, b) => {
-    return a + b ;
-}
-console.log(soma(3,5));
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './App'
+
+render(<App />, document.getElementById('app'));
